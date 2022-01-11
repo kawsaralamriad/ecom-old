ec8a80f7321ac43119b38601b2818f498dcda130
kawsaralamriad <kawsaralam282225@gmail.com>
2022-01-11 17:48:55 +0600

    backend templeat parting done

---
+++ backend/dashboard.php
@@ -0,0 +1,14 @@
+<?php include_once "partials/header.php"; ?>
+
+    <div id="app">
+       <?php include_once 'dashboard/side_bar.php';?>
+        <div id="main">
+            <header class="mb-3">
+                <a href="#" class="burger-btn d-block d-xl-none">
+                    <i class="bi bi-justify fs-3"></i>
+                </a>
+            </header>
+            <?php include_once'dashboard/page.php';?>
+      
+ <?php include_once'partials/footer.php';?>
+   
